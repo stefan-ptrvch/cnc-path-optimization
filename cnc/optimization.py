d833bf4001885e5ab555ebfda64cdd3efdfd8533
Stefan <petzimir@gmail.com>
2019-04-23 01:53:37 +0200

    Added a wrapper class that runs however many instances of the optimizer are needed. Implemented line grouping, as well as line/recipe grouping. Currently working on optimizing the order of line or line/recipe groups (the last optimization instance). Not sure if it's needed tho. Next step will be to improve the visualization, and after that, the algorithm itself.

--- cnc/optimization.py
+++ cnc/optimization.py
@@ -1,25 +1,279 @@
 import csv
 import numpy as np
+from multiprocessing import Process, Manager
+from cnc.visualization import Visualizer
 
 
-class ShortestPath():
+class CNCOptimizer():
+    """
+    Parses the input file, and generates a bunch of ShortestPath optimizers,
+    for different line types and for recipe groupings, if it's required. It
+    then runs the optimizers in parallel and saves the result.
+    """
+
+    def __init__(self, file_path, recipe_grouping=False):
+        """
+        Initializes the object.
+        """
+
+        # Path to file which contains line coordinates which need to be
+        # optimized
+        self.file_path = file_path
+
+        # Whether to do recipe grouping
+        self.recipe_grouping = recipe_grouping
+
+        # Generate dictionary of nodes
+        self.generate_nodes_from_file()
+
+
+    def generate_nodes_from_file(self):
+        """
+        Parses input file and generates the list of nodes that need to be
+        optimized.
+
+        Nodes are represented by 3D arrays of size (1, 2, 2), where the 2nd
+        dimension is the start/end poin and the 3rd is the x/y coordinate of
+        the point.
+
+        The list of nodes is represented by a 3D array of size (N, 2, 2) where
+        N represents the total number of nodes (total number of lines in file).
+        """
+
+        # Dictionary which contains numpy arrays of nodes, for every line type.
+        # If we need recipe grouping, we add subdictionaries, which hold the
+        # recipe nodes
+        self.nodes = {}
+
+        with open(self.file_path, 'r') as path_file:
+            reader = csv.reader(path_file, delimiter=' ')
+            for row in reader:
+                # Add line type to dictionary
+                if row[0] not in self.nodes.keys():
+                    if self.recipe_grouping:
+                        self.nodes[row[0]] = {}
+                    else:
+                        self.nodes[row[0]] = np.ndarray((0, 2, 2))
+
+                # If we use recipe grouping, check if we have this recipe for
+                # this line type
+                if self.recipe_grouping:
+                    if row[5] not in self.nodes[row[0]].keys():
+                        self.nodes[row[0]][row[5]] = np.ndarray((0, 2, 2))
+
+                # The coordinates in the file are Y1, X1, Y2, X2, and we
+                # want to store them as X1, Y1, X2, Y2
+                # We skip the 1st entry since it contains the type of line
+                node = np.array([
+                    float(row[2].replace(',', '')),
+                    float(row[1].replace(',', '')),
+                    float(row[4].replace(',', '')),
+                    float(row[3].replace(',', '')),
+                    ]).reshape((1, 2, 2))
+
+                # Add the node to the existing nodes
+                if self.recipe_grouping:
+                    self.nodes[row[0]][row[5]] = np.vstack((
+                        self.nodes[row[0]][row[5]],
+                        node
+                        ))
+                else:
+                    self.nodes[row[0]] = np.vstack((self.nodes[row[0]], node))
+
+
+    def optimize(self):
+        """
+        Runs optimizations for all the line types in parallel.
+        """
+
+        # Size of population per generation
+        pop_size = 10
+
+        # Probability of reproduction
+        repro = 0.8
+
+        # Probability of crossover
+        crossover = 0.9
+
+        # Probability of mutation
+        mutation = 0.001
+
+        # Number of generations to evolve
+        num_generations = 10
+
+        # Whether to remember best population, for debugging/visualization
+        debug = False
+
+        # List containing all optimization processes
+        processes = []
+
+        # Manager which will take care of shared state (all the optimization
+        # objects)
+        process_manager = Manager()
+        all_optimizations = process_manager.dict()
+
+        # For every line type, and recipe number (if we use recipe grouping),
+        # start an optimization
+        if self.recipe_grouping:
+            for line_type in self.nodes.keys():
+                for recipe_number in self.nodes[line_type].keys():
+
+                    # If there's not more that one line of this line/recipe
+                    # type, don't optimize it, because we will use it in the
+                    # "final" optimization run, when we optimize the
+                    # optimizations together
+                    print(line_type + recipe_number)
+                    if self.nodes[line_type][recipe_number].shape[0] > 1:
+                        p = Process(target=self.start_process, args=(
+                            all_optimizations,
+                            line_type + recipe_number,
+                            self.nodes[line_type][recipe_number],
+                            pop_size,
+                            repro,
+                            crossover,
+                            mutation,
+                            num_generations,
+                            debug
+                            ))
+                        processes.append(p)
+                        p.start()
+        else:
+            for line_type in self.nodes.keys():
+
+                # If there's not more that one line of this line type, don't
+                # optimize it, because we will use it in the "final" optimization
+                # run, when we optimize the optimizations together
+                if self.nodes[line_type].shape[0] > 1:
+                    p = Process(target=self.start_process, args=(
+                        all_optimizations,
+                        line_type,
+                        self.nodes[line_type],
+                        pop_size,
+                        repro,
+                        crossover,
+                        mutation,
+                        num_generations,
+                        debug
+                        ))
+                    processes.append(p)
+                    p.start()
+
+        # Wait for processes to finish before executing other code
+        for process in processes:
+            process.join()
+
+        ### DEBUG
+        for optimization in all_optimizations:
+            print("====================")
+            print(optimization)
+            print(all_optimizations[optimization].best_result)
+            print("====================")
+
+        # Run the last optimization, which optimizes the best paths of all
+        # subgroups
+        # These "group nodes" will be the starting point of the first line of
+        # the best path, and the ending point of the last line of the best
+        # path, so that we practically treat the whole "best path" of an
+        # optimization as one line, and then we optimize all the "best paths"
+        # of all groups together, as a set of line, since it does not matter
+        # how the tool end up where it is (we expect that it did the least
+        # ammount of travelling possible)
+
+        # This is the empty group nodes array
+        self.group_nodes = np.ndarray((0, 2, 2))
+
+        # Go through all the results, and extract the start and finish of the
+        # optimization path
+        for optimizer in all_optimizations:
+
+            # Get the best path
+            solution = all_optimizations[optimizer].best_result['solution']
+
+            # Extract the beginning and the end of the best path and treat it
+            # as one node (one line)
+            node = np.array([
+                all_optimizations[optimizer].nodes[solution[0]][0],
+                all_optimizations[optimizer].nodes[solution[-1]][1]
+                ]).reshape((1, 2, 2))
+
+            # Now add it to the list of nodes
+            self.group_nodes = np.vstack((self.group_nodes, node))
+
+        # Now, add all the lines types/recipe groupings of which there are only
+        # one (which could not be optimized)
+        # We also track the number of non optimized lines
+        self.num_single_lines = 0
+        if self.recipe_grouping:
+            for line_type in self.nodes:
+                for recipe_number in self.nodes[line_type]:
+                    if self.nodes[line_type][recipe_number].shape[0] == 1:
+                        self.num_single_lines += 1
+                        self.group_nodes = np.vstack((
+                            self.group_nodes,
+                            self.nodes[line_type][recipe_number]
+                            ))
+        else:
+            for line_type in self.nodes:
+                if self.nodes[line_type].shape[0] == 1:
+                    self.num_single_lines += 1
+                    self.group_nodes = np.vstack((
+                        self.group_nodes,
+                        self.nodes[line_type]
+                        ))
+
+        ### DEBUG
+        self.all_optimizations = all_optimizations
+
+        opt = GeneticAlgorithm(self.group_nodes, pop_size, repro, crossover,
+                mutation, num_generations, debug)
+
+        # Start the optimization
+        opt.optimize()
+        self.opt = opt
+
+
+    def start_process(self, all_optimizations, node_name, node, pop_size,
+            repro, crossover, mutation, num_generations, debug):
+        """
+        Method that creates initializes the optimization object, starts the
+        optimization and saves the result.
+        """
+
+        # Initialize the optimization object
+        opt = GeneticAlgorithm(node, pop_size, repro, crossover, mutation,
+                num_generations, debug)
+
+        # Start the optimization
+        opt.optimize()
+
+        # Save the result (optimized object)
+        all_optimizations[node_name] = opt
+
+
+    def visualize(self):
+        """
+        Visualizes the result of the optimization.
+        """
+        return
+        viz = Visualizer(opt)
+        #  viz.visualize_solution()
+
+
+class GeneticAlgorithm():
     """
     Finds the shortest cutting tool travel path (SCTTP) using the genetic
     algorithm.
     """
 
-    def __init__(self, file_path, pop_size, repro, crossover, mutation,
+    def __init__(self, nodes, pop_size, repro, crossover, mutation,
             num_generations, debug=False):
         """
         Initializes the optimization object with all the needed values to run
         the optimization.
         """
 
-        # Path to file containing code of CNC path
-        self.file_path = file_path
-
         # Get the nodes
-        self.generate_nodes_from_file()
+        self.nodes = nodes
 
         # Number of genes per individual
         self.num_genes = len(self.nodes)
@@ -113,33 +367,6 @@ class ShortestPath():
 
         # Calculate the fitness
         self.fitness = self.num_genes*self.max_distance - self.path_cost
-
-
-    def generate_nodes_from_file(self):
-        """
-        Parses input file and generates the list of nodes that need to be
-        optimized.
-
-        Nodes are represented by 3D arrays of size (1, 2, 2), where the 2nd
-        dimension is the start/end poin and the 3rd is the x/y coordinate of
-        the point.
-
-        The list of nodes is represented by a 3D array of size (N, 2, 2) where
-        N represents the total number of nodes (total number of lines in file).
-        """
-
-        self.nodes = np.ndarray((0, 2, 2))
-        with open(self.file_path, 'r') as path_file:
-            reader = csv.reader(path_file, delimiter=' ')
-            for row in reader:
-                if 'SCRIBE_LINE' == row[0]:
-                    node = np.array([
-                        float(row[1].replace(',', '')),
-                        float(row[2].replace(',', '')),
-                        float(row[3].replace(',', '')),
-                        float(row[4].replace(',', '')),
-                        ]).reshape((1, 2, 2))
-                    self.nodes = np.vstack((self.nodes, node))
 
 
     def reproduction(self):
@@ -262,7 +489,7 @@ class ShortestPath():
             self.evaluate_generation()
 
             ## DEBUG
-            print(generation, self.path_cost.mean())
+            #  print(generation, self.path_cost.mean())
             ## DEBUG
 
             # If we are debuggin/visualizing, remember the run
--- run.py
+++ run.py
@@ -7,41 +7,16 @@ importlib.reload(optimization)
 import cnc.visualization as visualization
 importlib.reload(visualization)
 
-from cnc.optimization import ShortestPath
-from cnc.visualization import Visualizer
+from cnc.optimization import CNCOptimizer
 
-
-### Initialize the optimization object
 # Path to input file
-path_file = './path_files/p1_stpl001.code'
+path_file = './path_files/p3_stpl001.code'
 
-# Size of population per generation
-pop_size = 100
+# Generate optimization object
+opt = CNCOptimizer(path_file, recipe_grouping=False)
 
-# Probability of reproduction
-repro = 0.8
-
-# Probability of crossover
-crossover = 0.9
-
-# Probability of mutation
-mutation = 0.001
-
-# Number of generations to evolve
-#  num_generations = 1000
-num_generations = 2
-
-# Whether to remember best population, for debugging/visualization
-debug = True
-
-opt = ShortestPath(
-        path_file, pop_size, repro, crossover, mutation, num_generations,
-        debug
-        )
-
-### Run the optimization
+# Run the optimization
 opt.optimize()
 
-### Plot the results
-viz = Visualizer(opt)
-viz.visualize_solution()
+# Generate visualization files
+opt.visualize()
